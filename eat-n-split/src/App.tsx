13383668d36fc15029d7296b2d6ca364f65ca2a6
nemmip <julkazbs2000@gmail.com>
2025-03-14 21:57:05 +0100

    Add eat-n-split-project setup and List of Friends

---
+++ eat-n-split/src/App.tsx
@@ -0,0 +1,13 @@
+import FriendsList from "./components/FriendsList.tsx";
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="sidebar">
+                <FriendsList/>
+            </div>
+        </div>
+    );
+};
+
+export default App;
