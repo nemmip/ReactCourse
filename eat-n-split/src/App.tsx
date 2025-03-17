9c4c6d72f51147c921116cd0fa8c6d85c10f4df6
nemmip <julkazbs2000@gmail.com>
2025-03-17 22:37:36 +0100

    Display and close add new friend form

--- eat-n-split/src/App.tsx
+++ eat-n-split/src/App.tsx
@@ -2,14 +2,22 @@ import FriendsList from "./components/Friend/FriendsList.tsx";
 import FormAddFriend from "./components/Friend/FormAddFriend.tsx";
 import Button from "./components/Button.tsx";
 import FormSplitBill from "./components/FormSplitBill.tsx";
+import {useState} from "react";
 
 const App = () => {
+    const [showAddFriend, setShowAddFriend] = useState(false);
+
+    const handleSetShowAddFriend = () => {
+        setShowAddFriend(show=> !show);
+    }
     return (
         <div className="app">
             <div className="sidebar">
                 <FriendsList/>
-                <FormAddFriend/>
-                <Button>Add friend</Button>
+                {showAddFriend && <FormAddFriend/>}
+                <Button onClick={handleSetShowAddFriend}>
+                    {showAddFriend ? 'Close':  'Add friend'}
+                </Button>
             </div>
             <FormSplitBill/>
         </div>
--- eat-n-split/src/components/Button.tsx
+++ eat-n-split/src/components/Button.tsx
@@ -1,7 +1,13 @@
 import React, {ReactNode} from 'react';
 
-const Button: React.FC<{children: ReactNode}> = ({children}) => {
-    return <button className="button">{children}</button>;
+const Button: React.FC<{
+    children: ReactNode;
+    onClick: () => void;
+}> = ({children, onClick}) => {
+    return <button
+        className="button"
+        onClick={onClick}
+    >{children}</button>;
 };
 
 export default Button;
