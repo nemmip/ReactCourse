9a6097f1b09eba91f5c56808348c8db496477bc5
nemmip <julkazbs2000@gmail.com>
2025-05-14 21:24:41 +0200

    Finish star component

--- usepopcorn/src/components/main/star/StarRating.tsx
+++ usepopcorn/src/components/main/star/StarRating.tsx
@@ -11,26 +11,36 @@ const starContainerStyle = {
     display: 'flex'
 }
 
-const textStyle = {
-    lineHeight: '1',
-    margin: '0'
-}
 
 const StarRating: React.FC<{
-    maxRating: number
-}> = ({maxRating = 5}) => {
-    const [rating, setRating] = useState<number>(0)
+    maxRating?: number,
+    color?: string,
+    size?: number,
+    className?: string,
+    messages?: string[],
+    defaultRating?: number,
+    onSetRating?: React.Dispatch<React.SetStateAction<number>>
+}> = ({onSetRating, maxRating = 5, color = '#fcc419', size = 48, className="", messages = [], defaultRating = 0}) => {
+    const [rating, setRating] = useState<number>(defaultRating);
     const [tempRating, setTempRating] = useState<number>(0)
+    const textStyle = {
+        lineHeight: '1',
+        margin: '0',
+        color,
+        fontSize: `${size/1.5}px`,
+    }
 
     const handleRating = (rating:number) => {
         setRating(rating)
+        if (onSetRating)
+            onSetRating(rating)
     }
     const handleTempRating = (rating:number) => {
         setTempRating(rating)
     }
 
     return (
-        <div style={containerStyle}>
+        <div style={containerStyle} className={className}>
             <div style={starContainerStyle}>
                 {Array.from({length: maxRating}, (_, i) =>
                     <Star
@@ -39,9 +49,13 @@ const StarRating: React.FC<{
                         onRate={() => handleRating(i+1)}
                         onHoverIn={() => handleTempRating(i+1)}
                         onHoverOut={() => handleTempRating(0)}
+                        color={color}
+                        size={size}
                     />)}
             </div>
-            <p style={textStyle}>{tempRating || rating || ""}</p>
+            <p style={textStyle}>{messages.length === maxRating ? messages[
+                tempRating ? tempRating - 1 : rating-1]
+                : tempRating || rating || ""}</p>
         </div>
     );
 };
--- usepopcorn/src/main.tsx
+++ usepopcorn/src/main.tsx
@@ -1,12 +1,20 @@
-import { StrictMode } from 'react'
+import {StrictMode, useState} from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App.tsx'
 import StarRating from "./components/main/star/StarRating.tsx";
-
+function Test () {
+    const [moveRating, setMoveRating] = useState<number>(0)
+    return <div>
+        <StarRating color={"blue"} maxRating={10} onSetRating={setMoveRating} />
+        <p>This movie was rated {moveRating} stars</p>
+    </div>
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/*<App />*/}
-      <StarRating maxRating={10}/>
+      <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+      <StarRating size={24} color={"red"} className={"test"} defaultRating={3}/>
+      <Test/>
   </StrictMode>,
 )
